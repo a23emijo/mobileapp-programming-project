bd9d10eb7f295a2c9c32cbf4058d8a6bc1fb62b1
a23emijo <a23emijo@student.his.se>
2024-05-13 10:15:17 +0200

    Created a working RecyclerView and added a placerholder text in the toolbar

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23emijo";
+
+    private ArrayList<Animal> Animals;
+
+    private RecyclerView recView;
+
+    private RecyclerViewAdapter recViewAdapter;
+
+    private Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Animals = new ArrayList<>(Arrays.asList(
+                new Animal("Zebra"),
+                new Animal("Hund"),
+                new Animal("Björn")
+        ));
+
+        recViewAdapter = new RecyclerViewAdapter(this, Animals);
+
+        recView = findViewById(R.id.recycler_view);
+        recView.setLayoutManager(new LinearLayoutManager(this));
+        recView.setAdapter(recViewAdapter);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        Type type = new TypeToken<ArrayList<Animal>>() {}.getType();
+        ArrayList<Animal> listOfMountains = gson.fromJson(json, type);
+
+        recViewAdapter.updateAdapter(listOfMountains);
+
+        recViewAdapter.notifyDataSetChanged();
     }
 
 }
---
+++ app/src/main/java/com/example/project/RecyclerViewAdapter.java
@@ -0,0 +1,53 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Animal> items;
+    private LayoutInflater layoutInflater;
+
+    RecyclerViewAdapter(Context context, List<Animal> items) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.items = items;
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(items.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+        }
+    }
+
+    public void updateAdapter(ArrayList<Animal> newItems){
+        items.addAll(newItems);
+    }
+}
